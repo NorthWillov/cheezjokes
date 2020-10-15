b06af8759632a384be4694e0bd9f396a2110f43a
Artem <artem@artemsMac.local>
2020-10-15 17:44:02 +0200

    Updates

---
+++ precache-manifest.dd7da44dfa2a803fcc9227110732a0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefed34773a671c42773bfa4e8fcd89a",
+    "url": "/colorboxes/index.html"
+  },
+  {
+    "revision": "5feaf6605a61860df57a",
+    "url": "/colorboxes/static/css/main.7b2c1e77.chunk.css"
+  },
+  {
+    "revision": "55774833f9eb18a7bb8d",
+    "url": "/colorboxes/static/js/2.02a1d08b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/colorboxes/static/js/2.02a1d08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5feaf6605a61860df57a",
+    "url": "/colorboxes/static/js/main.616fc1fc.chunk.js"
+  },
+  {
+    "revision": "caf93a1016755191957e",
+    "url": "/colorboxes/static/js/runtime-main.d7e377f9.js"
+  }
+]);
